29a7db7d77c09f9c1e6ff5873fecca6f7f580823
rana-ji0001 <karanrana705021@gmail.com>
2025-08-19 19:41:20 +0530

    created a login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,9 @@ const User = require("../models/User")
 const router = express.Router();
 const {body, validationResult } = require('express-validator');
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "ranaBad$boy";
   
 //create user using post req :: no login required res= api/auth/createuser
 router.post("/createuser" ,[
@@ -28,13 +31,56 @@ router.post("/createuser" ,[
         password: secPass,
     })
     // show the created user in json format 
-    res.json(user)
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    // console.log(jwtData);
+    res.json({authtoken});
     } catch (error) {
         // when some error occured in my code (no one knows what can happen)
         console.log(error.message);
-        res.status(500).send("some error occured");
+        res.status(500).send("Itnernal server error");
   }
+    
+
 })
+
+//Authenticate a  user using post req :: no login required res= api/auth/login
+    router.post("/login" ,[
+    body('email','enter a valid email').isEmail(),
+    body('password','password can not be blank').exists(),
+
+    ],async (req,res) => {
+      const result = validationResult(req);
+    if (!result.isEmpty()) {
+        return res.status(400).json({result:result.array()});
+    }
+    const {email,password} = req.body;
+      try {
+      let user = await User.findOne({email});
+      if(!user){
+        return res.status(400).json({error:"Please try to login with correct cradential"});
+      }
+      const passwordCompare = await bcrypt.compare(password,user.password);
+      if(!passwordCompare){
+        return res.status(400).json({error:"Please try to login with correct cradential"});
+      }
+      const data = {
+      user:{
+        id: user.id
+      }
+    }
+    const authtoken = jwt.sign(data,JWT_SECRET);
+    // console.log(jwtData);
+    res.json({authtoken});
+    } catch(error){
+      console.log(error.message);
+      res.status(500).send("Itnernal server error");
+    }
+    });
 
 
 module.exports = router
