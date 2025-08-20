f75c8a50c8daf391ec71b8abfd85345137bc2b02
rana-ji0001 <karanrana705021@gmail.com>
2025-08-20 23:57:44 +0530

    Fetching user's notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
+
+
   
 
-router.get("/" ,(req,res) => {
+router.get("/fetchallnotes" ,fetchuser, async (req,res) => {
 
-    res.json([]);
+    const notes = await Notes.find({user:req.user.id});
+    res.json(notes);
 })
 
 
