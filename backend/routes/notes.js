60d07c3e51079543e8b67b4fe8e3c645450f933f
rana-ji0001 <karanrana705021@gmail.com>
2025-08-24 13:14:29 +0530

    Can update same login user note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -39,5 +39,33 @@ router.post("/addnote" ,fetchuser,[
             console.log(error.message);
             res.status(500).send("Itnernal server error");
     }
+});
+
+
+//update an existing note using POST : //api/notes/updatenote , login required;
+router.put("/updatenote/:id" ,fetchuser, async (req,res) => {
+    const {title,description,tag} = req.body;
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+    
+    //find the user which needs update and then update it
+    let note =  await Notes.findById(req.params.id);
+
+    if(!note){
+        return res.status(404).send("Not Found");
+    }
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id , {$set:newNote} ,{new:true});
+    res.json({note});
+
+
+
+
+
 })
 module.exports = router
